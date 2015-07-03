d0fd098d559b08b0ba9b6ba013b0dca2e4ce0374
Gabriele Grillo <gabry.grillo@alice.it>
2015-07-03 14:58:02 +0200

    Initial commit

--- update.php
+++ update.php
@@ -1,20 +1,22 @@
 <?php
 include("Telegram.php");
 
-
+// Set the bot TOKEN
 $bot_id = "bot_token";
+// Instances the class
 $telegram = new Telegram($bot_id);
-
+// Get the data from Telegram
 $result = $telegram->getData();
 
-
+// Take text and chat_id from the message
 $text = $result["message"] ["text"];
 $chat_id = $result["message"] ["chat"]["id"];
 
-
+// Check if the text is a command
 if ($text == "/git") {
-	$reply = "Check me on GitHub: https://github.com/Eleirbag89/TelegramBotPHP";
-	$content = array('chat_id' => $chat_id, 'text' => $reply);
+    $reply = "Check me on GitHub: https://github.com/Eleirbag89/TelegramBotPHP";
+    // Build the reply array
+    $content = array('chat_id' => $chat_id, 'text' => $reply);
     $telegram->sendMessage($content);
 }
 
