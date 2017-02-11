aab0a971958181681ef54d4b40d39278e3c8db12
Gabriele Grillo <gabry.grillo@alice.it>
2017-02-11 12:02:34 +0100

    added buildInlineKeyboard doc

--- search/all_2.js
+++ search/all_2.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['buildforcereply',['buildForceReply',['../class_telegram.html#a34cfc8575c348ca3dd168b9611af4f1c',1,'Telegram']]],
   ['buildinlinekeyboard',['buildInlineKeyBoard',['../class_telegram.html#a81212e5e1fe0b3f72dd503a71577bbc4',1,'Telegram']]],
-  ['buildinlinekeyboardbutton',['buildInlineKeyboardButton',['../class_telegram.html#acb9d27401f6095212d6baf7d40ee1981',1,'Telegram']]],
+  ['buildinlinekeyboardbutton',['buildInlineKeyboardButton',['../class_telegram.html#a01432df36d1939f6c8dc09501f4ffcd3',1,'Telegram']]],
   ['buildkeyboard',['buildKeyBoard',['../class_telegram.html#a5c9619923ae1810aa340fa0e9acc1744',1,'Telegram']]],
   ['buildkeyboardbutton',['buildKeyboardButton',['../class_telegram.html#a611a52a2e9be838207b3943007a76b6c',1,'Telegram']]],
   ['buildkeyboardhide',['buildKeyBoardHide',['../class_telegram.html#a3a02a8bbaf6021f98871752bfd32de97',1,'Telegram']]]
