b285da115eb37457518e647c6341da02ce6c5668
Gabriele Grillo <gabry.grillo@alice.it>
2017-03-03 07:31:20 +0100

    Documentation updated

--- doc/html/search/functions_6.js
+++ doc/html/search/functions_6.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['firstname',['FirstName',['../class_telegram.html#a0e3e32188dd1631ed1ed673ac844d999',1,'Telegram']]],
-  ['forwardmessage',['forwardMessage',['../class_telegram.html#aebf3d567e947a457971c1501be7fc1fe',1,'Telegram']]]
+  ['forwardmessage',['forwardMessage',['../class_telegram.html#aebf3d567e947a457971c1501be7fc1fe',1,'Telegram']]],
+  ['fromchatid',['FromChatID',['../class_telegram.html#a4974bfd879b102b7bff6b69e14673b56',1,'Telegram']]],
+  ['fromid',['FromID',['../class_telegram.html#af9d816f01fd16f3ace634023f8320aad',1,'Telegram']]]
 ];
