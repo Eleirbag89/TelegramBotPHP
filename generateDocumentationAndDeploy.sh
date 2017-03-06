f7e5849948fe28e879998f90b9994e24995118ec
Gabriele Grillo <gabry.grillo@alice.it>
2017-03-07 00:06:17 +0100

    Move html dir to root repo folder

--- generateDocumentationAndDeploy.sh
+++ generateDocumentationAndDeploy.sh
@@ -79,7 +79,9 @@ doxygen $DOXYFILE 2>&1 | tee doxygen.log
 # Check this by verifying that the html directory and the file html/index.html
 # both exist. This is a good indication that Doxygen did it's work.
 if [ -d "html" ] && [ -f "html/index.html" ]; then
-
+    echo 'moving files'
+    mv  html/* .
+    rm html
     echo 'Uploading documentation to the gh-pages branch...'
     # Add everything in this directory (the Doxygen code documentation) to the
     # gh-pages branch.
