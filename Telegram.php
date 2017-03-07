6e9ff2477f8d7c5f237ea176d3a4f21282bec46e
Moein <tje3d@yahoo.com>
2017-03-07 21:24:15 +0330

    Curl File Create support for php < 5.5

--- Telegram.php
+++ Telegram.php
@@ -1568,3 +1568,12 @@ class Telegram {
     }
 
 }
+
+// Helper for Uploading file using CURL
+if (!function_exists('curl_file_create')) {
+    function curl_file_create($filename, $mimetype = '', $postname = '') {
+        return "@$filename;filename="
+                . ($postname ? : basename($filename))
+                . ($mimetype ? ";type=$mimetype" : '');
+    }
+}
