f6fc12a4f436543b4f011435e6ac5f3cd4939f26
Travis CI <travis@travis-ci.org>
2017-03-19 07:58:03 +0000

    Deploy code docs to GitHub Pages Travis build: 22
    Commit: 7cb6d61c2214777bb13d3fbe70a621e0c407600b

--- search/all_a.js
+++ search/all_a.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['lastname',['LastName',['../class_telegram.html#acf2c54b7fd06550cd64cf72194709a2c',1,'Telegram']]],
   ['leavechat',['leaveChat',['../class_telegram.html#ab5d2552e116deb2f9c3940f3e09b4e7a',1,'Telegram']]],
-  ['location',['Location',['../class_telegram.html#aae456ae24175445c9c797b0d58d5716b',1,'Telegram']]]
+  ['location',['Location',['../class_telegram.html#aae456ae24175445c9c797b0d58d5716b',1,'Telegram']]],
+  ['log',['log',['../class_telegram_error_logger.html#aeca7b590a3b91784d338f961098c8e85',1,'TelegramErrorLogger']]]
 ];
