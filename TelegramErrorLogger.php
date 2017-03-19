723115ab10251db979ad3f0ecc933b29fae2faaf
Gabriele Grillo <gabry.grillo@alice.it>
2017-03-19 08:41:09 +0100

    Added documentation comment

--- TelegramErrorLogger.php
+++ TelegramErrorLogger.php
@@ -1,8 +1,16 @@
 <?php
-
+/**
+ * Telegram Error Logger Class.
+ * @author shakibonline
+ */
 class TelegramErrorLogger
 {
-
+    /// Log request and response parameters from/to Telegrsm API
+    /**
+     * Prints the list of parameters from/to Telegram's API endpoint
+     * \param $result the Telegram's response as array
+     * \param $content the request parameters as array
+     */
     static public function log($result,$content)
     {
         try {
@@ -31,12 +39,12 @@ class TelegramErrorLogger
         }
     }
 
+    /// Write a string in the log file adding the current server time
     /**
-     * @param $fileName
-     * @param $array
-     * @param string $path
+     * Write a string in the log file TelegramErrorLogger.txt adding the current server time
+     * \param $error_text the text to append in the log
      */
-    private function _log_to_file($array)
+    private function _log_to_file($error_text)
     {
         try {
             $fileName = __CLASS__ . '.txt';
@@ -44,7 +52,7 @@ class TelegramErrorLogger
             $date = "============[Date]============";
             $date .= "\n";
             $date .= '[ ' . date('Y-m-d H:i:s  e') . ' ] ';
-            fwrite($myFile, $date .$array . "\n\n");
+            fwrite($myFile, $date .$error_text . "\n\n");
             fclose($myFile);
         } catch (\Exception $e) {
             echo $e->getMessage();
