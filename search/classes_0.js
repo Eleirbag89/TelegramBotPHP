c414423fb76e169022c5c99ac0d18b991097e256
Travis CI <travis@travis-ci.org>
2017-03-19 07:12:20 +0000

    Deploy code docs to GitHub Pages Travis build: 21
    Commit: 2e5bb189bfcccaef7fb2b47a9df7aeb1630e71bc

--- search/classes_0.js
+++ search/classes_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['telegram',['Telegram',['../class_telegram.html',1,'']]]
+  ['telegram',['Telegram',['../class_telegram.html',1,'']]],
+  ['telegramerrorlogger',['TelegramErrorLogger',['../class_telegram_error_logger.html',1,'']]]
 ];
