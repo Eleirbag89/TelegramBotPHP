fd2425167120bbad13d3d924b2e21b69f2c022cf
Abbas Shakiba <shakiba_9@yahoo.com>
2017-04-30 17:13:44 +0430

    Add rt function to logger class (#61)
    * Add rt function to logger class
    
    * merge rt function into telegramErrorLog
    
    * Remove varDump file

--- TelegramErrorLogger.php
+++ TelegramErrorLogger.php
@@ -1,7 +1,9 @@
 <?php
+
+
 /**
  * Telegram Error Logger Class.
- * @author shakibonline
+ * @author shakibonline <shakiba_9@yahoo.com>
  */
 class TelegramErrorLogger
 {
@@ -11,7 +13,7 @@ class TelegramErrorLogger
      * \param $result the Telegram's response as array
      * \param $content the request parameters as array
      */
-    static public function log($result,$content)
+    static public function log($result,$content, $use_rt = true)
     {
         try {
             if ($result['ok'] === false) {
@@ -26,8 +28,12 @@ class TelegramErrorLogger
                 }
                 $array = "=========[Sent Data]==========";
                 $array .= "\n";
-                foreach ($content as $key => $value) {
-                    $array .= $key . ":\t\t" . $value ."\n";
+                if ($use_rt == true) {
+                    $array .= $self->rt($content);
+                } else {
+                    foreach ($content as $key => $value) {
+                        $array .= $key . ":\t\t" . $value ."\n";
+                    }
                 }
                 $backtrace = "============[Trace]===========";
                 $backtrace .= "\n";
@@ -59,4 +65,32 @@ class TelegramErrorLogger
         }
     }
 
+
+    private function rt($array, $title = null, $head = true)
+    {
+        $ref = 'ref';
+        $text = '';
+        if ($head) {
+            $text = "[$ref]";
+            $text .= "\n";
+        }
+        foreach ($array as $key => $value) {
+            if (is_array($value)) {
+                if ($title != null) {
+                    $key = $title.'.'.$key;
+                }
+                $text .= rt($value , $key, false);
+            } else {
+                if (is_bool($value)) {
+                    $value = ($value) ? 'true' : 'false';
+                }
+                if ($title != '')
+                    $text .= $ref . '.'.$title.'.'.$key.'= '.$value.PHP_EOL;
+                else
+                    $text .= $ref . '.'.$key.'= '.$value.PHP_EOL;
+
+            }
+        }
+        return $text;
+    }
 }
