46c3b2183fae6d5dbe84699c899f052c819f8d46
Travis CI <travis@travis-ci.org>
2017-05-01 18:25:06 +0000

    Deploy code docs to GitHub Pages Travis build: 33
    Commit: 9836ca8f546045e08e558a45501a63171fa74b3c

--- search/all_7.js
+++ search/all_7.js
@@ -8,5 +8,6 @@ var searchData=
   ['getfile',['getFile',['../class_telegram.html#acb201ffc034063cfd347962c49ad5ad9',1,'Telegram']]],
   ['getme',['getMe',['../class_telegram.html#a6bc057a9b0c03f8147a4294936b2494b',1,'Telegram']]],
   ['getupdates',['getUpdates',['../class_telegram.html#adb467486d3d5e83ce21a03e00f17b16c',1,'Telegram']]],
+  ['getupdatetype',['getUpdateType',['../class_telegram.html#a291f955d4bae8e89b42e0a762b9bcc33',1,'Telegram']]],
   ['getuserprofilephotos',['getUserProfilePhotos',['../class_telegram.html#af2245dc488830eb7add02c35e8dcf492',1,'Telegram']]]
 ];
