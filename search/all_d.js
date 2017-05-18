cc2a5c7131d73a78bb9ee82ca56f701a1500c792
Travis CI <travis@travis-ci.org>
2017-05-18 19:44:50 +0000

    Deploy code docs to GitHub Pages Travis build: 39
    Commit: 19b0e83e91ce1f912a4f172c1ee649447ddcda66

--- search/all_d.js
+++ search/all_d.js
@@ -4,6 +4,7 @@ var searchData=
   ['sendchataction',['sendChatAction',['../class_telegram.html#aa8c4c54f8f2fcbb53de050458fb176b7',1,'Telegram']]],
   ['sendcontact',['sendContact',['../class_telegram.html#a5ba4fd675a877d995bb0d7305aa56324',1,'Telegram']]],
   ['senddocument',['sendDocument',['../class_telegram.html#a25569ca763bd149891d86b89bc66cf54',1,'Telegram']]],
+  ['sendinvoice',['sendInvoice',['../class_telegram.html#ae2c4d7308afec34759ccafe4a4f83053',1,'Telegram']]],
   ['sendlocation',['sendLocation',['../class_telegram.html#ac395e843f13d36d70f57c2d26868a33f',1,'Telegram']]],
   ['sendmessage',['sendMessage',['../class_telegram.html#af1369d6caab77f71a051ab73fa642a3f',1,'Telegram']]],
   ['sendphoto',['sendPhoto',['../class_telegram.html#ac7adcbd9f46b77664f8b738b84f290e7',1,'Telegram']]],
--- search/functions_1.js
+++ search/functions_1.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['answercallbackquery',['answerCallbackQuery',['../class_telegram.html#a55fa1d6bbcca385e333b402894e8ca37',1,'Telegram']]],
-  ['answerinlinequery',['answerInlineQuery',['../class_telegram.html#aae2439e89943ccea30fd724ad5ae9a89',1,'Telegram']]]
+  ['answerinlinequery',['answerInlineQuery',['../class_telegram.html#aae2439e89943ccea30fd724ad5ae9a89',1,'Telegram']]],
+  ['answerprecheckoutquery',['answerPreCheckoutQuery',['../class_telegram.html#a9eb2fa2623f2cd4f360f5b13fc4bfc73',1,'Telegram']]],
+  ['answershippingquery',['answerShippingQuery',['../class_telegram.html#a7f8d89bb5061feb40973d2ac31b10a5a',1,'Telegram']]]
 ];
