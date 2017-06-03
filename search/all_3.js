006d7fd3d9cc0e1b7143108c3872e01ab9ecf41a
Travis CI <travis@travis-ci.org>
2017-06-03 22:41:45 +0000

    Deploy code docs to GitHub Pages Travis build: 64
    Commit: 2a4e6c9a81e71b17ee958bfad8b84f098ea5ff14

--- search/all_3.js
+++ search/all_3.js
@@ -5,5 +5,6 @@ var searchData=
   ['callback_5fid',['Callback_ID',['../class_telegram.html#a61006cd19798be37ae51af5bc405ec71',1,'Telegram']]],
   ['callback_5fmessage',['Callback_Message',['../class_telegram.html#ad72a9632c75419625e9f194f400a31c9',1,'Telegram']]],
   ['callback_5fquery',['CALLBACK_QUERY',['../class_telegram.html#a464a4a095bb928388381b33e0babf9fe',1,'Telegram\CALLBACK_QUERY()'],['../class_telegram.html#a891f961aa0d3272c01ac9fe64f10a714',1,'Telegram\Callback_Query()']]],
-  ['chatid',['ChatID',['../class_telegram.html#a4b624bab3560ed1e761aba1e7431cb6e',1,'Telegram']]]
+  ['chatid',['ChatID',['../class_telegram.html#a4b624bab3560ed1e761aba1e7431cb6e',1,'Telegram']]],
+  ['contact',['CONTACT',['../class_telegram.html#aac7f4f1b55319f64ad56ae8a8222d9b5',1,'Telegram']]]
 ];
--- search/variables_1.js
+++ search/variables_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['callback_5fquery',['CALLBACK_QUERY',['../class_telegram.html#a464a4a095bb928388381b33e0babf9fe',1,'Telegram']]]
+  ['callback_5fquery',['CALLBACK_QUERY',['../class_telegram.html#a464a4a095bb928388381b33e0babf9fe',1,'Telegram']]],
+  ['contact',['CONTACT',['../class_telegram.html#aac7f4f1b55319f64ad56ae8a8222d9b5',1,'Telegram']]]
 ];
