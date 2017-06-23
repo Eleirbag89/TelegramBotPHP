8ebeb5c42dfc3d3b79f38788d13d93efdaa49e5e
Gabriele Grillo <gabry.grillo@alice.it>
2017-06-23 08:58:53 +0200

    Change var to global in testCase

--- tests/MessageTest.php
+++ tests/MessageTest.php
@@ -5,6 +5,8 @@ class MessageTest extends TestCase
 {
     public function testSendMessage()
     {
+        global $BOT_TOKEN;
+        global $CHAT_ID;
         $telegram = new Telegram($BOT_TOKEN);
         $reply = "It works!";
         $content = array('chat_id' => $CHAT_ID, 'text' => $reply);
