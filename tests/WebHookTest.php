b419ebfbfa9ffc4cc737761f53097bcde87fe31f
Gabriele Grillo <gabry.grillo@alice.it>
2017-06-26 20:40:51 +0200

    Test for WebHook

---
+++ tests/WebHookTest.php
@@ -0,0 +1,45 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class WebHookTest extends TestCase
+{
+    /**
+     * @dataProvider urlsProvider
+     */
+    public function testSetWebHook($url, $expectedBool, $expectedDesc)
+    {
+        global $BOT_TOKEN;
+        global $CHAT_ID;
+        global $WEBHOOK_URL;
+        $telegram = new Telegram($BOT_TOKEN);
+        $reply = $telegram->setWebhook($url);
+        $this->assertEquals($expectedBool, $reply['ok']);
+        $this->assertEquals($expectedDesc, $reply['description']);
+    }
+    
+      public function testDeleteWebHook()
+    {
+        global $BOT_TOKEN;
+        $telegram = new Telegram($BOT_TOKEN);
+        $reply = $telegram->deleteWebhook();
+        $this->assertTrue($reply['ok']);
+    }
+    
+    public function urlsProvider()
+    {
+        return [
+            ["test", false, "Bad Request: bad webhook: getaddrinfo: Name or service not known"],
+            ["http://google.com", false, "Bad Request: bad webhook: HTTPS url must be provided for webhook"],
+            ["https://google.com", true, "Webhook was set"],
+            ["", true, "Webhook was deleted"]
+        ];
+    }
+    
+    public static function tearDownAfterClass() {
+        parent::tearDownAfterClass();
+        global $WEBHOOK_URL;
+        $telegram = new Telegram($BOT_TOKEN);
+        $reply = $telegram->setWebhook($WEBHOOK_URL);
+    }
+}
+?>
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,3 +4,5 @@ global $BOT_TOKEN;
 $BOT_TOKEN = getenv('BOT_TOKEN');
 global $CHAT_ID;
 $CHAT_ID = getenv('CHAT_ID');
+global $WEBHOOK_URL;
+$WEBHOOK_URL = getenv('WEBHOOK_URL');
