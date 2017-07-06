c56eaea8464c1ea5196b26f09c3ca4002f71018a
caco3 <caco3@ruinelli.ch>
2017-07-06 22:37:48 +0200

    converted if's to elseif's to make it be quicker

--- bot examples/webhook/update.php
+++ bot examples/webhook/update.php
@@ -63,14 +63,15 @@ if(!is_null($text) && !is_null($chat_id)){
 		$content = array('chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => $reply);
 		$telegram->sendMessage($content);
 	}
-	if ($text == "/git") {
+	
+	elseif ($text == "/git") {
 	    $reply = "Check me on GitHub: https://github.com/Eleirbag89/TelegramBotPHP";
 	    // Build the reply array
 	    $content = array('chat_id' => $chat_id, 'text' => $reply);
 	    $telegram->sendMessage($content);
 	}
 	
-	if ($text == "/img") {
+	elseif ($text == "/img") {
 	    // Load a local file to upload. If is already on Telegram's Servers just pass the resource id
 	    $img = curl_file_create('test.png','image/png'); 
 	    $content = array('chat_id' => $chat_id, 'photo' => $img );
@@ -81,7 +82,7 @@ if(!is_null($text) && !is_null($chat_id)){
 	    $telegram->downloadFile($file["result"]["file_path"], "./test_download.png");
 	}
 	
-	if ($text == "/where") {
+	elseif ($text == "/where") {
 	    // Send the Catania's coordinate
 	    $content = array('chat_id' => $chat_id, 'latitude' => "37.5", 'longitude' => "15.1" );
 	    $telegram->sendLocation($content);
