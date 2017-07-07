85ac6b60f02d6521624a62c2ce86369c79d61b7d
Gabriele Grillo <gabry.grillo@alice.it>
2017-07-07 07:06:36 +0000

    Apply fixes from StyleCI

--- bot examples/updates/composer/error.php
+++ bot examples/updates/composer/error.php
@@ -1,16 +1,15 @@
 <?php
+
 require_once '../../../vendor/autoload.php';
 
-
-$bot_id = "bot_token";
+$bot_id = 'bot_token';
 $telegram = new Telegram($bot_id);
-
 
 $telegram->getUpdates();
 $telegram->serveUpdate(0);
 
 $res = $telegram->sendMessage([
     'chat_id' => 'adsf',  // Chat not found
-    'text' => 'Hello world'
+    'text'    => 'Hello world',
 ]);
 var_dump($res);
