7391855e409e61b4fdb960138203f1dc468f0cb1
Travis CI <travis@travis-ci.org>
2019-03-04 08:40:01 +0000

    Deploy code docs to GitHub Pages Travis build: 154
    Commit: 4cb023ac630bbc231ce22ecd25d64ba7bd42bf68

--- search/all_1.js
+++ search/all_1.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['addstickertoset',['addStickerToSet',['../class_telegram.html#a1dd9ac39878694581875b53f100189da',1,'Telegram']]],
+  ['animation',['ANIMATION',['../class_telegram.html#a07bcc5ee56f1ff193bf94341b7639f29',1,'Telegram']]],
   ['answercallbackquery',['answerCallbackQuery',['../class_telegram.html#a55fa1d6bbcca385e333b402894e8ca37',1,'Telegram']]],
   ['answerinlinequery',['answerInlineQuery',['../class_telegram.html#aae2439e89943ccea30fd724ad5ae9a89',1,'Telegram']]],
   ['answerprecheckoutquery',['answerPreCheckoutQuery',['../class_telegram.html#a9eb2fa2623f2cd4f360f5b13fc4bfc73',1,'Telegram']]],
