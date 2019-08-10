8287f6372b1edad07e5fff705dd13b4b916102ae
pershinHACKER <52803082+pershinHACKER@users.noreply.github.com>
2019-08-10 15:58:53 +0430

    mybot.php

---
+++ mybot.php
@@ -0,0 +1,11 @@
+include("Telegram.php");
+ 
+    date_default_timezone_set("asia/tehran");
+ 
+    // Set the bot 613894198:AAHn4TIW9FJbCaq8b2lp-hzstinKdX3FPD0
+ 
+    $bot_id = "613894198:AAHn4TIW9FJbCaq8b2lp-hzstinKdX3FPD0";
+ 
+    // Instances the class
+ 
+    $telegram = new Telegram($bot_id);
