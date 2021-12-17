3f6111eba3599db204f66d6c2253fa753e654a4d
jaenyf <95911656+jaenyf@users.noreply.github.com>
2021-12-17 11:57:12 +0100

    Add banChatMember method support

--- Telegram.php
+++ Telegram.php
@@ -1242,6 +1242,50 @@ class Telegram
         return $this->endpoint('leaveChat', $content);
     }
 
+    /// Ban Chat Member
+
+    /**
+     * Use this method to ban a user in a group, a supergroup or a channel. In the case of supergroups and channels, the user will not be able to return to the chat on their own using invite links, etc., unless unbanned first. The bot must be an administrator in the chat for this to work and must have the appropriate administrator rights. Returns <em>True</em> on success.<br/>Values inside $content:<br/>
+     * <table>
+     * <tr>
+     * <td><strong>Parameters</strong></td>
+     * <td><strong>Type</strong></td>
+     * <td><strong>Required</strong></td>
+     * <td><strong>Description</strong></td>
+     * </tr>
+     * <tr>
+     * <td>chat_id</td>
+     * <td>Integer or String</td>
+     * <td>Yes</td>
+     * <td>Unique identifier for the target group or username of the target supergroup (in the format <code>@supergroupusername</code>)</td>
+     * </tr>
+     * <tr>
+     * <td>user_id</td>
+     * <td>Integer</td>
+     * <td>Yes</td>
+     * <td>Unique identifier of the target user</td>
+     * </tr>
+     * <tr>
+     * <td>until_date</td>
+     * <td>Integer</td>
+     * <td>Optional</td>
+     * <td>Date when the user will be unbanned, unix time. If user is banned for more than <em366</em> days or less than <em>30</em> seconds from the current time they are considered to be banned forever. Applied for supergroups and channels only.</td>
+     * </tr>
+     * <tr>
+     * <td>revoke_messages</td>
+     * <td>Boolean</td>
+     * <td>Optional</td>
+     * <td>Pass <em>True</em> to delete all messages from the chat for the user that is being removed. If False, the user will be able to see messages in the group that were sent before the user was removed. Always True for supergroups and channels.</td>
+     * </tr>
+     * </table>
+     * \param $content the request parameters as array
+     * \return the JSON Telegram's reply.
+     */
+    public function banChatMember(array $content)
+    {
+        return $this->endpoint('banChatMember', $content);
+    }
+
     /// Unban Chat Member
 
     /**
